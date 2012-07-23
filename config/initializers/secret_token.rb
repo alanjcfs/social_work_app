37e4f53731e699b9a7fcba551a6f02115e7c75b9
Alan Fung-Schwarz <user@server>
2012-07-23 11:41:45 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SocialWorkApp::Application.config.secret_token = '396818800e75fcaef61590617b5d0250d3ab34c3df6c56f96f7dbd4c1d34de2a82b0ed804c9c6afba0b9166152c4e0a6816f94b163cda6dbea4d51d9ecf0a582'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+SocialWorkApp::Application.config.session_store :cookie_store, key: '_social_work_app_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# SocialWorkApp::Application.config.session_store :active_record_store
