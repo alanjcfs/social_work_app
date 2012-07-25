c2957fd925588548f89c5e06f88be7c7cb3f5256
Alan Schwarz <alan.jcfs@gmail.com>
2012-07-24 22:26:33 -0700

    commented out gender

--- db/migrate/20120725044754_add_more_details_to_client.rb
+++ db/migrate/20120725044754_add_more_details_to_client.rb
@@ -1,6 +1,6 @@
 class AddMoreDetailsToClient < ActiveRecord::Migration
   def change
     # add_column :clients, :case_completed, :date
-    add_column :clients, :gender, :string
+    # add_column :clients, :gender, :string
   end
 end
